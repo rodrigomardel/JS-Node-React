e169e1fc4d5e910444a2741215090c3001e2d11c
Rodrigo Martinez Delgado <rodrigo.mardel.daw@gmail.com>
2025-02-13 16:12:57 +0100

    rename folder ej1 - limpieza codigo app.js - update memoria

--- ejercicios/ej1_webNasa-module/version-modulos/app.js
+++ ejercicios/ej1_webNasa-module/version-modulos/app.js
@@ -3,8 +3,6 @@ import { obtenerDatos } from "./modules/datos.js";
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("buscar").addEventListener("click", (e) => {
     const fechaSeleccionada = document.getElementById("fecha").value;
-    console.log(fechaSeleccionada);
-
     obtenerDatos(fechaSeleccionada);
   });
 });
